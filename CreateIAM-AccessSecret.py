2dccc079336ff8a3188560a5e03266cece2bc4d5
Unni Krishnan S I <ukrishnan@vfirstt.com>
2023-10-31 12:38:47 +0530

    Added py file

---
+++ CreateIAM-AccessSecret.py
@@ -0,0 +1,36 @@
+import boto3
+
+def create_keys(username):
+    
+    # Initialize the IAM client
+    iam = boto3.client('iam')
+    
+    # Create access keys for the user
+    access_key_response = iam.create_access_key(UserName=username)
+    
+    # Return the access key and secret key
+    access_key = access_key_response['AccessKey']
+    return {
+        'AccessKeyId': access_key['AccessKeyId'],
+        'SecretAccessKey': access_key['SecretAccessKey']
+    }
+    
+def lambda_handler(event, context):
+    
+    # Specify the username for the IAM user
+    username = 'Steve' 
+    
+    # Function call to create the user and keys
+    result = create_keys(username)
+    
+    # Print the access key and secret key
+    print(f"Access Key ID: {result['AccessKeyId']}")
+    print(f"Secret Access Key: {result['SecretAccessKey']}")
+    
+    #Get the access key last used
+    # last_used = iam.get_access_key_last_used( AccessKeyId={result['AccessKeyId']})
+    # print(f"last Used: {last_used}")
+    
+    
+    # You can return the keys as a response, if needed
+    return result
